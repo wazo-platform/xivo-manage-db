bd87dce28a56c8c539deffc7bffb4ae5773abb34
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-01-20 11:24:52 -0500

    search for paths where pf-xivo was used in asterisk database

---
+++ xivo-manage-db/migration/asterisk-149.sql
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2014  Avencall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+BEGIN;
+
+UPDATE "musiconhold" SET "var_val" = replace("var_val", 'pf-xivo', 'xivo') WHERE "var_val" LIKE '%pf-xivo%';
+UPDATE "dialaction" SET "actionarg1" = replace("actionarg1", 'pf-xivo', 'xivo') WHERE "actionarg1" LIKE '%pf-xivo%';
+UPDATE "ctimain" SET "tlscertfile" = replace("tlscertfile", 'pf-xivo', 'xivo') WHERE "tlscertfile" LIKE '%pf-xivo%';
+UPDATE "ctimain" SET "tlsprivkeyfile" = replace("tlsprivkeyfile", 'pf-xivo', 'xivo') WHERE "tlsprivkeyfile" LIKE '%pf-xivo%';
+UPDATE "ctisheetactions" SET "sheet_qtui" = replace("sheet_qtui", 'pf-xivo', 'xivo') WHERE "sheet_qtui" LIKE '%pf-xivo%';
+
+COMMIT;

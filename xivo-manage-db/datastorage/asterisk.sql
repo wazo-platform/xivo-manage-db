57bbbbbd5414bac34fdc9977f84ad3cf783034db
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-02-10 11:00:13 -0500

    create tables for new function key schema

--- xivo-manage-db/datastorage/asterisk.sql
+++ xivo-manage-db/datastorage/asterisk.sql
@@ -931,6 +931,52 @@ INSERT INTO "features" VALUES (DEFAULT,1,0,0,'features.conf','featuremap','disco
 INSERT INTO "features" VALUES (DEFAULT,1,0,0,'features.conf','featuremap','automon','*3');
 INSERT INTO "features" VALUES (DEFAULT,1,0,0,'features.conf','featuremap','atxfer','*2');
 
+DROP TABLE IF EXISTS "func_key_template" CASCADE;
+CREATE TABLE "func_key_template" (
+    "id"        SERIAL          PRIMARY KEY,
+    "name"      VARCHAR(128)    NOT NULL,
+    "private"   BOOLEAN         NOT NULL    DEFAULT FALSE
+);
+
+DROP TABLE IF EXISTS "func_key_type" CASCADE;
+CREATE TABLE "func_key_type" (
+    "id"        SERIAL          PRIMARY KEY,
+    "name"      VARCHAR(128)    NOT NULL
+);
+
+INSERT INTO "func_key_type" ("name") VALUES ('speeddial');
+
+DROP TABLE IF EXISTS "func_key_destination_type" CASCADE;
+CREATE TABLE "func_key_destination_type" (
+    "id"        SERIAL          PRIMARY KEY,
+    "name"      VARCHAR(128)    NOT NULL
+);
+
+INSERT INTO "func_key_destination_type" (id, name) VALUES (1, 'user');
+
+DROP TABLE IF EXISTS "func_key" CASCADE;
+CREATE TABLE "func_key" (
+    "id"                    SERIAL          NOT NULL,
+    "type_id"               INTEGER         NOT NULL    REFERENCES "func_key_type"("id"),
+    "destination_type_id"   INTEGER         NOT NULL    REFERENCES "func_key_destination_type"("id"),
+
+    PRIMARY KEY ("id", "destination_type_id")
+);
+
+DROP TABLE IF EXISTS "func_key_mapping" CASCADE;
+CREATE TABLE "func_key_mapping" (
+    "template_id"               INTEGER         REFERENCES "func_key_template"("id"),
+    "func_key_id"               INTEGER         NOT NULL,
+    "destination_type_id"       INTEGER         NOT NULL,
+    "label"                     VARCHAR(128)    NULL,
+    "position"                  INTEGER         NOT NULL,
+    "blf"                       BOOLEAN         NOT NULL    DEFAULT FALSE,
+
+    PRIMARY KEY("template_id", "func_key_id"),
+    FOREIGN KEY("func_key_id", "destination_type_id") REFERENCES "func_key"("id", "destination_type_id"),
+    UNIQUE("template_id", "position"),
+    CHECK("position" >= 0)
+);
 
 DROP TABLE IF EXISTS "paging" CASCADE;
 CREATE TABLE "paging" (
@@ -2066,6 +2112,8 @@ CREATE TABLE "userfeatures" (
  "rightcallcode" VARCHAR(16),
  "commented" INTEGER NOT NULL DEFAULT 0, -- BOOLEAN
  "description" text NOT NULL,
+ "func_key_template_id" INTEGER NULL REFERENCES "func_key_template"("id"),
+ "func_key_private_template_id" INTEGER NULL REFERENCES "func_key_template"("id"),
  PRIMARY KEY("id")
 );
 
@@ -2077,6 +2125,16 @@ CREATE INDEX "userfeatures__idx__agentid" ON "userfeatures"("agentid");
 CREATE INDEX "userfeatures__idx__loginclient" ON "userfeatures"("loginclient");
 CREATE INDEX "userfeatures__idx__musiconhold" ON "userfeatures"("musiconhold");
 
+DROP TABLE IF EXISTS "func_key_dest_user" CASCADE;
+CREATE TABLE "func_key_dest_user" (
+    "func_key_id"               INTEGER         NOT NULL,
+    "user_id"                   INTEGER         NOT NULL        REFERENCES "userfeatures"("id"),
+    /* destination_type_id 1 = user (see table func_key_destination_type) */
+    "destination_type_id"       INTEGER         NOT NULL        DEFAULT 1       CHECK ("destination_type_id" = 1),
+
+    PRIMARY KEY("func_key_id", "user_id", "destination_type_id"),
+    FOREIGN KEY("func_key_id", "destination_type_id") REFERENCES "func_key"("id", "destination_type_id")
+);
 
 DROP TABLE IF EXISTS "useriax" CASCADE;
 DROP TABLE IF EXISTS "usersip" CASCADE;

62a2c246478fce98963f0e59c08d963675aab475
Etienne Lessard <elessard@avencall.com>
2014-05-09 09:50:02 -0400

    set stat_agent_periodic_id_seq to right value

---
+++ alembic/versions/108164f6117e_fix_sequence_usage_on_stat_agent_.py
@@ -0,0 +1,23 @@
+"""fix sequence usage on stat_agent_periodic
+
+Revision ID: 108164f6117e
+Revises: 52bd5977c09d
+Create Date: 2014-05-09 09:17:37.551108
+XiVO Version: 14.08
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '108164f6117e'
+down_revision = '52bd5977c09d'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.execute("SELECT setval('stat_agent_periodic_id_seq', (SELECT max(id) FROM stat_agent_periodic));")
+
+
+def downgrade():
+    pass

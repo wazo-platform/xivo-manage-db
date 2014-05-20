ad50193f0234801d2b11f0a2cb2efa601f6d60fb
Cedric Abunar <cabunar@avencall.com>
2014-05-20 13:08:33 -0400

    fix context field not populated in queue table

---
+++ alembic/versions/447e4ab61975_fix_queue_table_without_context.py
@@ -0,0 +1,32 @@
+"""fix_queue_table_without_context
+
+Revision ID: 447e4ab61975
+Revises: 146ddf6c4b36
+Create Date: 2014-05-20 11:17:26.779671
+XiVO Version: <version>
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '447e4ab61975'
+down_revision = '146ddf6c4b36'
+
+from alembic import op
+from sqlalchemy.sql import table, column
+from sqlalchemy import String
+
+
+def upgrade():
+    qry = """UPDATE queue SET context = queuefeatures.context from queuefeatures WHERE queuefeatures.name = queue.name"""
+    op.execute(qry)
+    qry = """UPDATE queue SET context = groupfeatures.context from groupfeatures WHERE groupfeatures.name = queue.name"""
+    op.execute(qry)
+
+
+def downgrade():
+    queue = table('queue',
+        column('context', String)
+    )
+    op.execute(
+        queue.update().values({'context': ''})
+    )

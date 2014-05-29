f1c3fb8b5e20724ef07d4b9ad185d88a2d3da034
Cedric Abunar <cabunar@avencall.com>
2014-05-29 09:27:12 -0400

    add migration script for entity_filter

---
+++ alembic/versions/1f4cbd713979_add_missing_entity_id_column.py
@@ -0,0 +1,37 @@
+"""add_entity_to_callfilter
+
+Revision ID: 1f4cbd713979
+Revises: 6bfd932df2c
+Create Date: 2014-05-28 14:42:23.479319
+XiVO Version: <version>
+
+"""
+
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.types import Integer
+from sqlalchemy import text
+
+# revision identifiers, used by Alembic.
+revision = '1f4cbd713979'
+down_revision = '6bfd932df2c'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('callfilter',
+                  Column('entity_id', Integer, server_default=text('NULL'), ForeignKey('entity.id'))
+    )
+    op.add_column('pickup',
+                  Column('entity_id', Integer, server_default=text('NULL'), ForeignKey('entity.id'))
+    )
+    op.add_column('schedule',
+                  Column('entity_id', Integer, server_default=text('NULL'), ForeignKey('entity.id'))
+    )
+
+
+def downgrade():
+    op.drop_column('callfilter', 'entity_id')
+    op.drop_column('pickup', 'entity_id')
+    op.drop_column('schedule', 'entity_id')

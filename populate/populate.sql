cbe1b14351ccec77f814fe260d7bca81c057c3be
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-06-11 16:25:38 -0400

    rewrite query to select calls after an overflow
    calls that overflow towards another queue do not emit ENTERQUEUE
    events in the new queue. This causes a bug with the first subquery
    (select "time" from queue_log) since it can't find the beginning
    of the call anymore

--- populate/populate.sql
+++ populate/populate.sql
@@ -651,32 +651,46 @@ CREATE FUNCTION "fill_answered_calls"(period_start text, period_end text)
   RETURNS void AS
 $$
   INSERT INTO stat_call_on_queue (callid, "time", talktime, waittime, queue_id, agent_id, status)
+
+  WITH call_start AS (
   SELECT
-    outer_queue_log.callid,
-    CAST ((SELECT "time"
-           FROM queue_log
-           WHERE callid=outer_queue_log.callid AND
-                 queuename=outer_queue_log.queuename AND
-                 event='ENTERQUEUE' ORDER BY "time" DESC LIMIT 1) AS TIMESTAMP) AS "time",
-    CASE WHEN event IN ('COMPLETEAGENT', 'COMPLETECALLER') THEN CAST (data2 AS INTEGER)
-         WHEN event = 'TRANSFER' THEN CAST (data4 AS INTEGER) END as talktime,
-    CASE WHEN event IN ('COMPLETEAGENT', 'COMPLETECALLER') THEN CAST (data1 AS INTEGER)
-         WHEN event = 'TRANSFER' THEN CAST (data3 AS INTEGER) END as waittime,
-    stat_queue.id AS queue_id,
-    stat_agent.id AS agent_id,
-    'answered' AS status
+      callid as callid,
+      time::TIMESTAMP as started_at
   FROM
-    queue_log as outer_queue_log
-  LEFT JOIN
-    stat_agent ON outer_queue_log.agent = stat_agent.name
-  LEFT JOIN
-    stat_queue ON outer_queue_log.queuename = stat_queue.name
+      queue_log
   WHERE
-    callid IN
-      (SELECT callid
-       FROM queue_log
-       WHERE event = 'ENTERQUEUE' AND "time" BETWEEN $1 AND $2)
-    AND event IN ('COMPLETEAGENT', 'COMPLETECALLER', 'TRANSFER');
+      event = 'ENTERQUEUE'
+      AND time::TIMESTAMP BETWEEN $1::TIMESTAMP AND $2::TIMESTAMP
+  )
+
+  SELECT
+      call_start.callid,
+      call_start.started_at AS "time",
+      CASE
+          WHEN queue_log.event IN ('COMPLETEAGENT', 'COMPLETECALLER')
+            THEN CAST (queue_log.data2 AS INTEGER)
+          WHEN queue_log.event = 'TRANSFER'
+            THEN CAST (queue_log.data4 AS INTEGER)
+      END as talktime,
+      CASE
+          WHEN queue_log.event IN ('COMPLETEAGENT', 'COMPLETECALLER')
+            THEN CAST (queue_log.data1 AS INTEGER)
+          WHEN queue_log.event = 'TRANSFER'
+            THEN CAST (queue_log.data3 AS INTEGER)
+      END as waittime,
+      stat_queue.id AS queue_id,
+      stat_agent.id AS agent_id,
+      'answered' AS status
+  FROM
+      queue_log
+  INNER JOIN
+      call_start ON call_start.callid = queue_log.callid
+  LEFT JOIN
+      stat_agent ON queue_log.agent = stat_agent.name
+  LEFT JOIN
+      stat_queue ON queue_log.queuename = stat_queue.name
+  WHERE
+      event IN ('COMPLETEAGENT', 'COMPLETECALLER', 'TRANSFER')
 $$
 LANGUAGE SQL;
 ALTER FUNCTION "fill_answered_calls" (period_start text, period_end text) OWNER TO asterisk;

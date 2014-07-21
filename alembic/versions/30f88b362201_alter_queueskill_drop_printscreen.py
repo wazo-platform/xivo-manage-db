7740f7638c8531ef881f05abe455d90edc8c4c87
Etienne Lessard <elessard@avencall.com>
2014-07-21 14:50:12 -0400

    drop column printscreen on table queueskill

---
+++ alembic/versions/30f88b362201_alter_queueskill_drop_printscreen.py
@@ -0,0 +1,24 @@
+"""alter queueskill drop printscreen
+
+Revision ID: 30f88b362201
+Revises: 31eecc16c5c5
+Create Date: 2014-07-21 14:38:56.835026
+XiVO Version: 14.14
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '30f88b362201'
+down_revision = '31eecc16c5c5'
+
+from alembic import op
+from sqlalchemy.schema import Column
+from sqlalchemy.types import String
+
+
+def upgrade():
+    op.drop_column('queueskill', 'printscreen')
+
+
+def downgrade():
+    op.add_column('queueskill', Column('printscreen', String(5)))

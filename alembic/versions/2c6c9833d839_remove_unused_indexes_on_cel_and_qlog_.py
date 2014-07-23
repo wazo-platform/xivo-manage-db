df98873b29348c680c921362f4a861bd3088fc38
Cedric Abunar <cabunar@avencall.com>
2014-07-23 11:09:27 -0400

    remove unused indexes on cel and qlog tables

---
+++ alembic/versions/2c6c9833d839_remove_unused_indexes_on_cel_and_qlog_.py
@@ -0,0 +1,23 @@
+"""remove unused indexes on cel and qlog tables
+
+Revision ID: 2c6c9833d839
+Revises: 30f88b362201
+XiVO Version: <version>
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2c6c9833d839'
+down_revision = '30f88b362201'
+
+from alembic import op
+
+
+def upgrade():
+    op.drop_index('cel__idx__uniqueid')
+    op.drop_index('queue_log__idx_queuename')
+
+
+def downgrade():
+    op.create_index('cel__idx__uniqueid', 'cel', ['uniqueid'])
+    op.create_index('queue_log__idx_queuename', 'queue_log', ['queuename'])

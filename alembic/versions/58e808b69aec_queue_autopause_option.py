106015efdb358a0cd9e13e3901b229dc3dd3a332
Etienne Lessard <elessard@avencall.com>
2014-08-06 10:57:17 -0400

    set default value for autopause to no

--- alembic/versions/58e808b69aec_queue_autopause_option.py
+++ alembic/versions/58e808b69aec_queue_autopause_option.py
@@ -2,7 +2,7 @@
 
 Revision ID: 58e808b69aec
 Revises: 2c6c9833d839
-XiVO Version: <version>
+XiVO Version: 14.15
 
 """
 
@@ -16,22 +16,20 @@ from alembic import op
 def upgrade():
     qry = """ALTER TABLE queue ALTER COLUMN autopause TYPE varchar(3);"""
     op.execute(qry)
-    qry = """ALTER TABLE queue ALTER COLUMN autopause SET DEFAULT 'yes';"""
+    qry = """ALTER TABLE queue ALTER COLUMN autopause SET DEFAULT 'no';"""
     op.execute(qry)
     qry = """UPDATE queue SET autopause='yes' WHERE autopause='1';"""
     op.execute(qry)
     qry = """UPDATE queue SET autopause='no' WHERE autopause='0';"""
     op.execute(qry)
-    qry = """ALTER TABLE queue ADD CONSTRAINT queue_autopause CHECK (autopause IN ('yes','no','all'));"""
+    qry = """ALTER TABLE queue ADD CONSTRAINT queue_autopause CHECK (autopause IN ('no','yes','all'));"""
     op.execute(qry)
 
 
 def downgrade():
     qry = """ALTER TABLE queue DROP CONSTRAINT queue_autopause;"""
     op.execute(qry)
-    qry = """UPDATE queue SET autopause='1' WHERE autopause='yes';"""
-    op.execute(qry)
-    qry = """UPDATE queue SET autopause='1' WHERE autopause='all';"""
+    qry = """UPDATE queue SET autopause='1' WHERE autopause in ('yes','all');"""
     op.execute(qry)
     qry = """UPDATE queue SET autopause='0' WHERE autopause='no';"""
     op.execute(qry)

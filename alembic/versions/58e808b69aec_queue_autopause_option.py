d01299ff9d7c729409d8376c94d3e466a45947b3
Cedric Abunar <cabunar@avencall.com>
2014-08-06 10:14:45 -0400

    update queue autopause option
    The autopause option in queues.conf can be passed a new value, "all." The
    result is that if a member becomes auto-paused, he will be paused in all
    queues for which he is a member, not just the queue that failed to reach
    the member
    ; Autopause will pause a queue member if they fail to answer a call
    ; no: Member will not be paused
    ; yes: Member will be paused only in the queue where the timeout took place
    ; all: Memeber will be paused in all queues he/she is a member
    ;autopause=yes

---
+++ alembic/versions/58e808b69aec_queue_autopause_option.py
@@ -0,0 +1,41 @@
+"""queue_autopause_option
+
+Revision ID: 58e808b69aec
+Revises: 2c6c9833d839
+XiVO Version: <version>
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '58e808b69aec'
+down_revision = '2c6c9833d839'
+
+from alembic import op
+
+
+def upgrade():
+    qry = """ALTER TABLE queue ALTER COLUMN autopause TYPE varchar(3);"""
+    op.execute(qry)
+    qry = """ALTER TABLE queue ALTER COLUMN autopause SET DEFAULT 'yes';"""
+    op.execute(qry)
+    qry = """UPDATE queue SET autopause='yes' WHERE autopause='1';"""
+    op.execute(qry)
+    qry = """UPDATE queue SET autopause='no' WHERE autopause='0';"""
+    op.execute(qry)
+    qry = """ALTER TABLE queue ADD CONSTRAINT queue_autopause CHECK (autopause IN ('yes','no','all'));"""
+    op.execute(qry)
+
+
+def downgrade():
+    qry = """ALTER TABLE queue DROP CONSTRAINT queue_autopause;"""
+    op.execute(qry)
+    qry = """UPDATE queue SET autopause='1' WHERE autopause='yes';"""
+    op.execute(qry)
+    qry = """UPDATE queue SET autopause='1' WHERE autopause='all';"""
+    op.execute(qry)
+    qry = """UPDATE queue SET autopause='0' WHERE autopause='no';"""
+    op.execute(qry)
+    qry = """ALTER TABLE queue ALTER COLUMN autopause SET DEFAULT 1;"""
+    op.execute(qry)
+    qry = """ALTER TABLE queue ALTER COLUMN autopause TYPE integer USING (autopause::integer);"""
+    op.execute(qry)

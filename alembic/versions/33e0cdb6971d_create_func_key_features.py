241c60493a2487d72c60c8462e2856531e5998c1
Gregory Eric Sanderson <gsanderson@avencall.com>
2014-09-19 10:43:33 -0400

    forgot to insert destination for parking func key

--- alembic/versions/33e0cdb6971d_create_func_key_features.py
+++ alembic/versions/33e0cdb6971d_create_func_key_features.py
@@ -14,6 +14,8 @@ from alembic import op
 import sqlalchemy as sa
 
 
+PARKING_TYPE = 'parkext'
+
 TRANSFER_TYPE_NAME = 'transfer'
 
 FEATURES_DESTINATION_ID = 8
@@ -37,6 +39,11 @@ func_key_dest_features_table = sa.sql.table('func_key_dest_features',
                                             sa.sql.column('func_key_id'),
                                             sa.sql.column('destination_type_id'),
                                             sa.sql.column('features_id'))
+
+features_table = sa.sql.table('features',
+                              sa.sql.column('id'),
+                              sa.sql.column('category'),
+                              sa.sql.column('var_name'))
 
 
 def upgrade():
@@ -82,13 +89,32 @@ def create_dest_features_table():
 
 
 def insert_parking_func_key(type_id, destination_type_id):
-    query = (func_key_table
-             .insert()
-             .returning(func_key_table.c.id)
-             .values(type_id=type_id,
-                     destination_type_id=destination_type_id))
+    func_key_query = (func_key_table
+                      .insert()
+                      .returning(func_key_table.c.id)
+                      .values(type_id=type_id,
+                              destination_type_id=destination_type_id))
 
-    return op.get_bind().execute(query).scalar()
+    func_key_id = op.get_bind().execute(func_key_query).scalar()
+
+    features_query = (
+        sa.sql.select(
+            [features_table.c.id])
+        .where(
+            sa.sql.and_(
+                features_table.c.category == 'general',
+                features_table.c.var_name == PARKING_TYPE)))
+
+    features_id = op.get_bind().execute(features_query).scalar()
+
+    dest_query = (func_key_dest_features_table
+                  .insert()
+                  .returning(func_key_dest_features_table.c.func_key_id)
+                  .values(func_key_id=func_key_id,
+                          destination_type_id=destination_type_id,
+                          features_id=features_id))
+
+    return op.get_bind().execute(dest_query).scalar()
 
 
 def downgrade():

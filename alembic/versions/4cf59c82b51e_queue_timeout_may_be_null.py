6d95337978fc75c6782cdad1851857974494efa8
Jean-Philippe Gelinas <jpgelinas@avencall.com>
2014-10-07 15:36:29 -0400

    when downgrading, convert queuefeatures.timeout to 0 if null
    queuefeatures.timeout is not null, null values would break downward migration

--- alembic/versions/4cf59c82b51e_queue_timeout_may_be_null.py
+++ alembic/versions/4cf59c82b51e_queue_timeout_may_be_null.py
@@ -10,6 +10,10 @@ revision = '4cf59c82b51e'
 down_revision = '234745874c55'
 
 from alembic import op
+import sqlalchemy as sa
+
+queuefeatures_table = sa.sql.table('queuefeatures',
+                                   sa.Column('timeout', sa.Integer))
 
 
 def upgrade():
@@ -17,4 +21,7 @@ def upgrade():
 
 
 def downgrade():
+    op.execute(queuefeatures_table.update().
+               where(queuefeatures_table.c.timeout == None).
+               values(timeout='0'))
     op.alter_column('queuefeatures', 'timeout', server_default='0', nullable=False)

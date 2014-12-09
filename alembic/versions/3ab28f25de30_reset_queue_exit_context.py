1e7cf6cc2f58f8e618dfe62f8f66acc0fd9d6d14
Etienne Lessard <elessard@avencall.com>
2014-12-09 15:13:26 -0500

    reset queue exit context to null where it make sense

---
+++ alembic/versions/3ab28f25de30_reset_queue_exit_context.py
@@ -0,0 +1,21 @@
+"""reset queue exit context
+
+Revision ID: 3ab28f25de30
+Revises: 3770e116222d
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3ab28f25de30'
+down_revision = '3770e116222d'
+
+from alembic import op
+
+
+def upgrade():
+    op.execute('''UPDATE queue SET context = null FROM queuefeatures WHERE queue.name = queuefeatures.name AND queue.context = queuefeatures.context''')
+    op.execute('''UPDATE queue SET context = null FROM groupfeatures WHERE queue.name = groupfeatures.name AND queue.context = groupfeatures.context''')
+
+
+def downgrade():
+    pass
--- alembic/versions/447e4ab61975_fix_queue_table_without_context.py
+++ alembic/versions/447e4ab61975_fix_queue_table_without_context.py
@@ -9,22 +9,10 @@ Revises: 146ddf6c4b36
 revision = '447e4ab61975'
 down_revision = '146ddf6c4b36'
 
-from alembic import op
-from sqlalchemy.sql import table, column
-from sqlalchemy import String
-
 
 def upgrade():
-    qry = """UPDATE queue SET context = queuefeatures.context from queuefeatures WHERE queuefeatures.name = queue.name"""
-    op.execute(qry)
-    qry = """UPDATE queue SET context = groupfeatures.context from groupfeatures WHERE groupfeatures.name = queue.name"""
-    op.execute(qry)
+    pass
 
 
 def downgrade():
-    queue = table('queue',
-        column('context', String)
-    )
-    op.execute(
-        queue.update().values({'context': ''})
-    )
+    pass

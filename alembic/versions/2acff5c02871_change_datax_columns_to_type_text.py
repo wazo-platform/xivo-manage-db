ebdbac5ee6558ae23010b4a002ebff91bb32891a
Etienne Lessard <elessard@avencall.com>
2015-07-10 13:51:04 -0400

    first change the column type, then the default value
    Otherwise the order is not defined, and when alembic/sqlalchemy decides
    to emit the "set default" before the "set data type", then the default
    value is not changed correctly (i.e. the old cast remains).

--- alembic/versions/2acff5c02871_change_datax_columns_to_type_text.py
+++ alembic/versions/2acff5c02871_change_datax_columns_to_type_text.py
@@ -14,11 +14,16 @@ from sqlalchemy.types import Text
 
 
 def upgrade():
-    op.alter_column('queue_log', 'data1', type_=Text, server_default='')
-    op.alter_column('queue_log', 'data2', type_=Text, server_default='')
-    op.alter_column('queue_log', 'data3', type_=Text, server_default='')
-    op.alter_column('queue_log', 'data4', type_=Text, server_default='')
-    op.alter_column('queue_log', 'data5', type_=Text, server_default='')
+    op.alter_column('queue_log', 'data1', type_=Text)
+    op.alter_column('queue_log', 'data2', type_=Text)
+    op.alter_column('queue_log', 'data3', type_=Text)
+    op.alter_column('queue_log', 'data4', type_=Text)
+    op.alter_column('queue_log', 'data5', type_=Text)
+    op.alter_column('queue_log', 'data1', server_default='')
+    op.alter_column('queue_log', 'data2', server_default='')
+    op.alter_column('queue_log', 'data3', server_default='')
+    op.alter_column('queue_log', 'data4', server_default='')
+    op.alter_column('queue_log', 'data5', server_default='')
 
 
 def downgrade():

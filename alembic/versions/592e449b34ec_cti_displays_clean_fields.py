f5ebcbd38bd740ff269970ab7c838e3bb6acf0c7
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-09-01 10:42:26 -0400

    cti_displays: remove all {db-*} and {xivo-*}
    only the column name is left

---
+++ alembic/versions/592e449b34ec_cti_displays_clean_fields.py
@@ -0,0 +1,45 @@
+"""cti_displays clean fields
+
+Revision ID: 592e449b34ec
+Revises: 2bb6543883a6
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '592e449b34ec'
+down_revision = '2bb6543883a6'
+
+import re
+
+from alembic import op
+from sqlalchemy import sql
+from functools import partial
+
+t = sql.table('ctidisplays',
+              sql.column('id'),
+              sql.column('data'))
+
+
+def sub_all(pattern, repl, string):
+    r = re.compile(pattern)
+    while True:
+        string, n = r.subn(repl, string)
+        if not n:
+            return string
+
+
+def upgrade():
+    conn = op.get_bind()
+    rows = conn.execute(sql.select([t]))
+
+    f = partial(sub_all, r'(.*)\{\w+-(\w+)\}(.*)', r'\1\2\3')
+    to_upgrade = {row.id: f(row.data) for row in rows}
+
+    for id_, value in to_upgrade.iteritems():
+        op.execute(t.update()
+                   .where(t.c.id == id_)
+                   .values(data=value))
+
+
+def downgrade():
+    pass

69e05530d544da9b7a9042f89b736f3fef267a70
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-09-02 08:33:40 -0400

    directories: add favorite and personal
    a new installation will have a column to display favorites and personnal

--- populate/populate.sql
+++ populate/populate.sql
@@ -197,7 +197,7 @@ INSERT INTO "ctidirectoryfields" VALUES(2, 'phone', '{exten}');
 INSERT INTO "ctidirectoryfields" VALUES(2, 'name', '{firstname} {lastname}');
 
 
-INSERT INTO "ctidisplays" VALUES(DEFAULT,'Display','{"10": [ "Nom","name","","name" ],"20": [ "Numéro","number","","phone" ],"30": [ "Entreprise","","Inconnue","company" ],"40": [ "E-mail","","","mail" ], "50": [ "Source","","","directory" ]}',1,'Affichage par défaut');
+INSERT INTO "ctidisplays" VALUES(DEFAULT,'Display','{"10": [ "Nom","name","","name" ],"20": [ "Numéro","number","","phone" ],"30": [ "Entreprise","","Inconnue","company" ],"40": [ "E-mail","","","mail" ], "50": ["Favoris", "favorite", "", ""], "60": ["", "personnal", "", ""], "70": [ "Source","","","directory" ]}',1,'Affichage par défaut');
 INSERT INTO "ctidisplays" VALUES(DEFAULT, 'switchboard', '{ "10": [ "", "status", "", ""],"20": [ "Name", "name", "", "name"],"30": [ "Number", "number_office", "", "phone"]}', 1, '');
 
 

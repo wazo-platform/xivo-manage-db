8f3783a6e59125c1a8ec180e2da67f77954f88ef
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-10-07 15:50:55 -0400

    remove constraints preventing seperation of line and endpoint

---
+++ alembic/versions/20d4630f2c8e_remove_constraints_from_linefeatures.py
@@ -0,0 +1,30 @@
+"""remove constraints from linefeatures
+
+Revision ID: 20d4630f2c8e
+Revises: 500ca962c3a8
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '20d4630f2c8e'
+down_revision = '500ca962c3a8'
+
+
+def upgrade():
+    op.alter_column('linefeatures', 'protocol', nullable=True)
+    op.alter_column('linefeatures', 'protocolid', nullable=True)
+    op.alter_column('linefeatures', 'name', nullable=True)
+    op.alter_column('linefeatures', 'num', server_default='1')
+    op.create_unique_constraint('linefeatures_provisioningid_key',
+                                "linefeatures",
+                                ['provisioningid'])
+
+
+def downgrade():
+    op.alter_column('linefeatures', 'protocol', nullable=False)
+    op.alter_column('linefeatures', 'protocolid', nullable=False)
+    op.alter_column('linefeatures', 'name', nullable=False)
+    op.alter_column('linefeatures', 'num', server_default='0')
+    op.drop_constraint('linefeatures_provisioningid_key', 'linefeatures')

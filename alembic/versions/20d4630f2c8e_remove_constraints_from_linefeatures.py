5990ab4a6bf32d58f0474f2177d26a5a68977704
Etienne Lessard <elessard@avencall.com>
2015-10-14 11:06:07 -0400

    don't add unique constraint on provisioningid
    Since it's not unique for "sccp" lines, where it's always 0.

--- alembic/versions/20d4630f2c8e_remove_constraints_from_linefeatures.py
+++ alembic/versions/20d4630f2c8e_remove_constraints_from_linefeatures.py
@@ -17,9 +17,6 @@ def upgrade():
     op.alter_column('linefeatures', 'protocolid', nullable=True)
     op.alter_column('linefeatures', 'name', nullable=True)
     op.alter_column('linefeatures', 'num', server_default='1')
-    op.create_unique_constraint('linefeatures_provisioningid_key',
-                                "linefeatures",
-                                ['provisioningid'])
 
 
 def downgrade():
@@ -27,4 +24,3 @@ def downgrade():
     op.alter_column('linefeatures', 'protocolid', nullable=False)
     op.alter_column('linefeatures', 'name', nullable=False)
     op.alter_column('linefeatures', 'num', server_default='0')
-    op.drop_constraint('linefeatures_provisioningid_key', 'linefeatures')

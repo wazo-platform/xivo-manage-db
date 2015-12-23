8dfd86ccf2d574773e1da18823d340f804cc0449
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-12-23 15:11:18 -0500

    add voicemail foreign key in user table

---
+++ alembic/versions/1d4b487e4d1f_add_voicemail_foreign_key.py
@@ -0,0 +1,22 @@
+"""add_voicemail_foreign_key
+
+Revision ID: 1d4b487e4d1f
+Revises: 3a0ace87fc76
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '1d4b487e4d1f'
+down_revision = '3a0ace87fc76'
+
+
+def upgrade():
+    op.create_foreign_key('userfeatures_voicemailid_fkey',
+                          'userfeatures', 'voicemail',
+                          ['voicemailid'], ['uniqueid'])
+
+
+def downgrade():
+    op.drop_constraint('userfeatures_voicemailid_fkey', 'userfeatures')

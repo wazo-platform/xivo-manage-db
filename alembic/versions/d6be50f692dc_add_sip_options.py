73462241612119b6ad3083f76ca89523c1f7996e
Gregory Eric Sanderson <gsanderson@avencall.com>
2016-01-15 11:49:57 -0500

    fix pep8 warnings

--- alembic/versions/d6be50f692dc_add_sip_options.py
+++ alembic/versions/d6be50f692dc_add_sip_options.py
@@ -5,18 +5,19 @@ Revises: 5524f5f02959
 
 """
 
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ARRAY
+
 # revision identifiers, used by Alembic.
 revision = 'd6be50f692dc'
 down_revision = '5524f5f02959'
 
-from alembic import op
-import sqlalchemy as sa
-
 
 def upgrade():
     op.add_column('usersip', sa.Column('options',
-                                         ARRAY(sa.String, dimensions=2),
-                                         nullable=False, server_default='{}'))
+                                       ARRAY(sa.String, dimensions=2),
+                                       nullable=False, server_default='{}'))
 
 
 def downgrade():

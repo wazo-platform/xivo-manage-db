1bd3d8868288bb12bce19b85b331dcf613acf34a
Lord Gregory Eric Sanderson Turcot Temlett MacDonnell of Glengary Forbes of Linden <gsanderson@proformatique.com>
2016-03-04 10:01:17 -0500

    add ACLS to agid for accessing confd

---
+++ alembic/versions/3420040c5650_add_confd_acls_to_agid.py
@@ -0,0 +1,47 @@
+"""add_confd_acls_to_agid
+
+Revision ID: 3420040c5650
+Revises: 4123465e114e
+
+"""
+
+from alembic import op
+from sqlalchemy import sql
+from sqlalchemy.dialects.postgres import ARRAY, VARCHAR
+
+# revision identifiers, used by Alembic.
+revision = '3420040c5650'
+down_revision = '4123465e114e'
+
+
+accesswebservice = sql.table('accesswebservice',
+                             sql.column('id'),
+                             sql.column('name'),
+                             sql.column('acl'))
+
+ACLS = ['confd.lines.read',
+        'confd.devices.read',
+        'confd.lines.*.devices.*.update',
+        'confd.devices.*.synchronize.read']
+
+
+def upgrade():
+    acls = sql.cast(ACLS, ARRAY(VARCHAR))
+    query = (accesswebservice
+             .update()
+             .values(
+                 acl=sql.func.array_cat(accesswebservice.c.acl, acls))
+             .where(
+                 accesswebservice.c.name == "xivo-agid"))
+    op.execute(query)
+
+
+def downgrade():
+    for acl in ACLS:
+        query = (accesswebservice
+                 .update()
+                 .values(
+                     acl=sql.func.array_remove(accesswebservice.c.acl, acl))
+                 .where(
+                     accesswebservice.c.name == "xivo-agid"))
+        op.execute(query)

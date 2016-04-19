4a32e88930555abd650b43423f701edfe4703928
Etienne Lessard <elessard@proformatique.com>
2016-04-19 11:09:01 -0400

    rebase down_revision

--- alembic/versions/2b0f5ee268c4_remove_orphan_lines.py
+++ alembic/versions/2b0f5ee268c4_remove_orphan_lines.py
@@ -1,13 +1,13 @@
 """remove orphan lines
 
 Revision ID: 2b0f5ee268c4
-Revises: 255de5d2adea
+Revises: 1b6be5d5e4ac
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '2b0f5ee268c4'
-down_revision = '255de5d2adea'
+down_revision = '1b6be5d5e4ac'
 
 from alembic import op
 from sqlalchemy import sql

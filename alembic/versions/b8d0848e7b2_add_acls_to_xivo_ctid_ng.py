6466aa7cced525a226ea85e3bacb50633ffbd489
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-05-26 11:43:26 -0400

    add_acls: avoid removing previous customization

--- alembic/versions/b8d0848e7b2_add_acls_to_xivo_ctid_ng.py
+++ alembic/versions/b8d0848e7b2_add_acls_to_xivo_ctid_ng.py
@@ -1,13 +1,13 @@
 """add ACLs to xivo-ctid-ng
 
 Revision ID: b8d0848e7b2
-Revises: 2c940e077157
+Revises: 3c9280e9ed5c
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'b8d0848e7b2'
-down_revision = '2c940e077157'
+down_revision = '3c9280e9ed5c'
 
 from alembic import op
 from sqlalchemy import sql
@@ -15,15 +15,22 @@ from sqlalchemy import sql
 
 webservice = sql.table('accesswebservice',
                        sql.column('name'),
-                       sql.column('login'),
-                       sql.column('passwd'),
-                       sql.column('acl'),
-                       sql.column('description'))
+                       sql.column('acl'))
+
+SERVICE = 'xivo-ctid-ng'
+NEW_ACL = set(['amid.action.ShowDialplan.create'])
 
 
 def upgrade():
-    _update_web_service_acl('xivo-ctid-ng',
-                            '{confd.#, amid.action.Redirect.create, amid.action.ShowDialplan.create, amid.action.Setvar.create}')
+    current_acls = _get_current_acl(SERVICE)
+    new_acls = current_acls | NEW_ACL
+    _update_web_service_acl(SERVICE, new_acls)
+
+
+def _get_current_acl(name):
+    return set(op.get_bind().execute(
+        sql.select([webservice.c.acl]).where(webservice.c.name == name)
+    ).scalar())
 
 
 def _update_web_service_acl(name, acl):
@@ -31,9 +38,10 @@ def _update_web_service_acl(name, acl):
                .update()
                .where(
                    webservice.c.name == name
-               ).values(acl=acl))
+               ).values(acl=list(acl)))
 
 
 def downgrade():
-    _update_web_service_acl('xivo-ctid-ng',
-                            '{confd.#, amid.action.Redirect.create, amid.action.Setvar.create}')
+    current_acls = _get_current_acl(SERVICE)
+    new_acls = current_acls - NEW_ACL
+    _update_web_service_acl(SERVICE, new_acls)

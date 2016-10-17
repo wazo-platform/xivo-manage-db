d604ab1c25ddcb0c49bd3cc7e6eaff51e62408da
Francois Blackburn <fblackburn@proformatique.com>
2016-10-17 09:52:00 -0400

    clean outcalltrunk table before adding fk
    reason: the webi does not delete outcalltrunk when a trunk is deleted.
    So we cannot add a foreign key if the table is not clean.

--- alembic/versions/4fa0a41c0327_add_foreignkey_outcalltrunk.py
+++ alembic/versions/4fa0a41c0327_add_foreignkey_outcalltrunk.py
@@ -10,9 +10,21 @@ revision = '4fa0a41c0327'
 down_revision = '4fd0315a61fc'
 
 from alembic import op
+from sqlalchemy import sql
+
+trunk_table = sql.table('trunkfeatures',
+                        sql.column('id'))
+
+outcall_table = sql.table('outcall',
+                          sql.column('id'))
+
+outcalltrunk_table = sql.table('outcalltrunk',
+                               sql.column('trunkfeaturesid'),
+                               sql.column('outcallid'))
 
 
 def upgrade():
+    _clean_outcalltrunk_table()
     op.create_foreign_key('outcalltrunk_outcallid_fkey',
                           'outcalltrunk', 'outcall',
                           ['outcallid'], ['id'])
@@ -21,6 +33,24 @@ def upgrade():
                           ['trunkfeaturesid'], ['id'])
     op.alter_column('outcalltrunk', 'outcallid', server_default=None)
     op.alter_column('outcalltrunk', 'trunkfeaturesid', server_default=None)
+
+
+def _clean_outcalltrunk_table():
+    all_trunks = sql.select([trunk_table.c.id]).alias()
+    query = (outcalltrunk_table
+             .delete()
+             .where(outcalltrunk_table.c.trunkfeaturesid.notin_(
+                 all_trunks)))
+
+    op.execute(query)
+
+    all_outcalls = sql.select([outcall_table.c.id]).alias()
+    query = (outcalltrunk_table
+             .delete()
+             .where(outcalltrunk_table.c.outcallid.notin_(
+                 all_outcalls)))
+
+    op.execute(query)
 
 
 def downgrade():

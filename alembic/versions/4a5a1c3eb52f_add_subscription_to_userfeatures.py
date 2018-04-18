f66bdfc1b2e9f762745eb7a63fcdbf51460533b9
Francois Blackburn <fblackburn@wazo.community>
2018-04-18 10:02:31 -0400

    userfeatures created_at add server_default

--- alembic/versions/4a5a1c3eb52f_add_subscription_to_userfeatures.py
+++ alembic/versions/4a5a1c3eb52f_add_subscription_to_userfeatures.py
@@ -17,7 +17,15 @@ down_revision = '68507181de97'
 
 
 def upgrade():
-    op.add_column('userfeatures', sa.Column('created_at', sa.DateTime, default=datetime.datetime.utcnow))
+    op.add_column(
+        'userfeatures',
+        sa.Column(
+            'created_at',
+            sa.DateTime,
+            default=datetime.datetime.utcnow,
+            server_default=sa.text("(now() at time zone 'utc')")
+        )
+    )
     op.add_column('userfeatures', sa.Column('subscription_type', sa.Integer, nullable=False, server_default='0'))
 
 

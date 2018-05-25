f2043d812f816560e4ac640cdab5ac5e885513be
Pascal Cadotte Michaud <pcm@wazo.io>
2018-05-25 14:11:45 -0400

    use the first entity as default during the context tenant_uuid migration
    contexts created using xivo-confd are not associated to an entity

--- alembic/versions/b5c40615bc21_context_add_the_tenant_uuid.py
+++ alembic/versions/b5c40615bc21_context_add_the_tenant_uuid.py
@@ -20,8 +20,12 @@ entity_table = sa.sql.table(
 )
 
 
-def associate_tenants():
+def associate_tenants(default_tenant):
     tbl = sa.sql.table('context', sa.sql.column('entity'), sa.sql.column('tenant_uuid'))
+
+    query = tbl.update().values(tenant_uuid=default_tenant)
+    op.execute(query)
+
     for name, uuid in get_entity_tenant_map().iteritems():
         query = tbl.update().where(tbl.c.entity == name).values(tenant_uuid=uuid)
         op.execute(query)
@@ -33,12 +37,19 @@ def get_entity_tenant_map():
     return {row.name: row.tenant_uuid for row in rows}
 
 
+def find_default_tenant_uuid():
+    query = sa.sql.select([entity_table.c.tenant_uuid])
+    for row in op.get_bind().execute(query):
+        return row.tenant_uuid
+
+
 def upgrade():
     op.add_column(
         'context',
         sa.Column('tenant_uuid', sa.String(36), sa.ForeignKey('tenant.uuid'), nullable=True),
     )
-    associate_tenants()
+    default_tenant = find_default_tenant_uuid()
+    associate_tenants(default_tenant)
     op.alter_column('context', 'tenant_uuid', nullable=False)
 
 

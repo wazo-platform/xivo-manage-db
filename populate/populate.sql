7078bfcd8f9048c3f41eb29490a52467a567eebd
Pascal Cadotte Michaud <pcm@wazo.io>
2018-06-19 08:09:26 -0400

    acl: add auth.tenants.read to wazo-call-logd

--- populate/populate.sql
+++ populate/populate.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-auth', 'wazo-auth', substring(gen_salt('bf',4),8), '{confd.users.read}');
 INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-dird-xivo-backend', 'wazo-dird-xivo-backend', substring(gen_salt('bf',4),8), '{confd.users.read, confd.infos.read}');
-INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-call-logd', 'wazo-call-logd', substring(gen_salt('bf',4),8), '{confd.lines.read, confd.users.*.read}');
+INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-call-logd', 'wazo-call-logd', substring(gen_salt('bf',4),8), '{auth.tenants.read, confd.lines.read, confd.users.*.read}');
 INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-plugind', 'wazo-plugind', substring(gen_salt('bf',4),8), '{confd.infos.read}');
 INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-plugind-cli', 'wazo-plugind-cli', substring(gen_salt('bf',4),8), '{plugind.#}');
 INSERT INTO "accesswebservice" (uuid, name, login, passwd, acl) VALUES (uuid_generate_v4(), 'wazo-upgrade', 'wazo-upgrade', substring(gen_salt('bf',4),8), '{plugind.#,confd.#,auth.#}');

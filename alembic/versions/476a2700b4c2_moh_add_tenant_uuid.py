7ceaf99ceea9c065d8918fbfd9b7e33aadfca100
Francois Blackburn <fblackburn@wazo.community>
2018-08-02 13:48:51 -0400

    add tenant_uuid to moh

---
+++ alembic/versions/476a2700b4c2_moh_add_tenant_uuid.py
@@ -0,0 +1,46 @@
+"""moh_add_tenant_uuid
+
+Revision ID: 476a2700b4c2
+Revises: 129646f8f81f
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '476a2700b4c2'
+down_revision = '129646f8f81f'
+
+TABLE = 'moh'
+
+
+def find_default_tenant_uuid():
+    entity_table = sa.sql.table(
+        'entity',
+        sa.sql.column('id'),
+        sa.sql.column('name'),
+        sa.sql.column('tenant_uuid'),
+    )
+    query = sa.sql.select([entity_table.c.tenant_uuid]).order_by(entity_table.c.id)
+    for row in op.get_bind().execute(query):
+        return row.tenant_uuid
+
+
+def upgrade():
+    default_tenant = find_default_tenant_uuid()
+    op.add_column(
+        TABLE,
+        sa.Column(
+            'tenant_uuid',
+            sa.String(36),
+            sa.ForeignKey('tenant.uuid', ondelete='CASCADE'),
+            nullable=False,
+            server_default=default_tenant),
+    )
+    op.alter_column(TABLE, 'tenant_uuid', server_default=None)
+
+
+def downgrade():
+    op.drop_column(TABLE, 'tenant_uuid')

f337df74c2d563863156014f359fbc8a6320bb32
Pascal Cadotte Michaud <pcm@wazo.io>
2018-09-06 11:52:26 -0400

    add auth.tenants.read to xivo-agid
    to be able to use multi-tenants routes the service needs to be able to read tenants in wazo-auth

---
+++ alembic/versions/5013b66e26d2_add_auth_tenants_read_to_agid.py
@@ -0,0 +1,45 @@
+"""add auth.tenants.read to agid
+
+Revision ID: 5013b66e26d2
+Revises: 0c84ec128899
+
+"""
+
+from alembic import op
+from sqlalchemy import sql
+from sqlalchemy.dialects.postgres import ARRAY, VARCHAR
+
+
+# revision identifiers, used by Alembic.
+revision = '5013b66e26d2'
+down_revision = '0c84ec128899'
+
+accesswebservice = sql.table('accesswebservice',
+                             sql.column('id'),
+                             sql.column('name'),
+                             sql.column('acl'))
+
+ACLS = ['auth.tenants.read']
+SERVICE = 'xivo-agid'
+
+
+def upgrade():
+    acls = sql.cast(ACLS, ARRAY(VARCHAR))
+    query = (accesswebservice
+             .update()
+             .values(
+                 acl=sql.func.array_cat(accesswebservice.c.acl, acls))
+             .where(
+                 accesswebservice.c.name == SERVICE))
+    op.execute(query)
+
+
+def downgrade():
+    for acl in ACLS:
+        query = (accesswebservice
+                 .update()
+                 .values(
+                     acl=sql.func.array_remove(accesswebservice.c.acl, acl))
+                 .where(
+                     accesswebservice.c.name == SERVICE))
+        op.execute(query)

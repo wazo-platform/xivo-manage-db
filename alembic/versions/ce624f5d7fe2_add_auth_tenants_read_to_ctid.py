47cb277d711edfe924578a97cbb382253875c419
Francois Blackburn <fblackburn@wazo.community>
2018-10-01 10:49:55 -0400

    add auth.tenants.read to xivo-ctid
    to be able to use multi-tenants route
    Note: users/uuid/services/* are not multi-tenants for now

---
+++ alembic/versions/ce624f5d7fe2_add_auth_tenants_read_to_ctid.py
@@ -0,0 +1,50 @@
+"""add_auth_tenants_read_to_ctid
+
+Revision ID: ce624f5d7fe2
+Revises: 778dc763148f
+
+"""
+
+from alembic import op
+from sqlalchemy import sql
+from sqlalchemy.dialects.postgres import ARRAY, VARCHAR
+
+# revision identifiers, used by Alembic.
+revision = 'ce624f5d7fe2'
+down_revision = '778dc763148f'
+
+accesswebservice = sql.table(
+    'accesswebservice',
+    sql.column('id'),
+    sql.column('name'),
+    sql.column('acl'),
+)
+
+ACLS = ['auth.tenants.read']
+SERVICE = 'xivo-ctid'
+
+
+def upgrade():
+    acls = sql.cast(ACLS, ARRAY(VARCHAR))
+    query = (
+        accesswebservice
+        .update()
+        .values(
+            acl=sql.func.array_cat(accesswebservice.c.acl, acls))
+        .where(
+            accesswebservice.c.name == SERVICE)
+    )
+    op.execute(query)
+
+
+def downgrade():
+    for acl in ACLS:
+        query = (
+            accesswebservice
+            .update()
+            .values(
+                acl=sql.func.array_remove(accesswebservice.c.acl, acl))
+            .where(
+                accesswebservice.c.name == SERVICE)
+        )
+        op.execute(query)

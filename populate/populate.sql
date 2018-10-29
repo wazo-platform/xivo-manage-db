c36f26b44949e53a1fddd61639e781c946c1c481
francois <francois@fblackburn.lan.wazo.io>
2018-10-29 08:34:43 -0400

    bump version (18.13)

--- populate/populate.sql
+++ populate/populate.sql
@@ -924,6 +924,6 @@ INSERT INTO "provisioning" VALUES(DEFAULT, '', '127.0.0.1', 'admin', 'admin', 0,
 
 /* The UUID "populate-uuid" will be replaced by pg-populate-db */
 /* The version is bumped automatically during the release process */
-INSERT INTO "infos" VALUES ('populate-uuid', '18.12');
+INSERT INTO "infos" VALUES ('populate-uuid', '18.13');
 
 COMMIT;

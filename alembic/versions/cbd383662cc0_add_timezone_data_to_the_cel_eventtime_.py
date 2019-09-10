7ddefef9eb4650d9687d4f8d8281e53346e093cb
Pascal Cadotte Michaud <pcm@wazo.io>
2019-09-10 13:21:29 -0400

    cel: add tz info to the eventtime column

---
+++ alembic/versions/cbd383662cc0_add_timezone_data_to_the_cel_eventtime_.py
@@ -0,0 +1,22 @@
+"""add timezone data to the cel.eventtime column
+
+Revision ID: cbd383662cc0
+Revises: fbbbf7d78ed0
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cbd383662cc0'
+down_revision = 'fbbbf7d78ed0'
+
+
+def upgrade():
+    op.alter_column('cel', 'eventtime', nullable=False, type_=sa.DateTime(timezone=True))
+
+
+def downgrade():
+    op.alter_column('cel', 'eventtime', nullable=False, type_=sa.DateTime())

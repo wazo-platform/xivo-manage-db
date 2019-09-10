2bf4030a95af914ef3b01cdd29bceb84adb3c120
Pascal Cadotte Michaud <pcm@wazo.io>
2019-09-10 14:14:33 -0400

    call-log: add timezone info

--- alembic/versions/cbd383662cc0_add_timezone_data_to_the_cel_eventtime_.py
+++ alembic/versions/cbd383662cc0_add_timezone_data_to_the_cel_eventtime_.py
@@ -16,7 +16,13 @@ down_revision = 'fbbbf7d78ed0'
 
 def upgrade():
     op.alter_column('cel', 'eventtime', nullable=False, type_=sa.DateTime(timezone=True))
+    op.alter_column('call_log', 'date', type_=sa.DateTime(timezone=True))
+    op.alter_column('call_log', 'date_answer', type_=sa.DateTime(timezone=True))
+    op.alter_column('call_log', 'date_end', type_=sa.DateTime(timezone=True))
 
 
 def downgrade():
+    op.alter_column('call_log', 'date', type_=sa.DateTime())
+    op.alter_column('call_log', 'date_answer', type_=sa.DateTime())
+    op.alter_column('call_log', 'date_end', type_=sa.DateTime())
     op.alter_column('cel', 'eventtime', nullable=False, type_=sa.DateTime())

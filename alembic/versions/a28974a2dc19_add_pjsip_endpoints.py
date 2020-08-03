4f688f8414e39dd97c892e8b25299fdc512482f6
Francois Blackburn <fblackburn@wazo.community>
2020-08-03 12:42:23 -0400

    rebase alembic

--- alembic/versions/a28974a2dc19_add_pjsip_endpoints.py
+++ alembic/versions/a28974a2dc19_add_pjsip_endpoints.py
@@ -1,7 +1,7 @@
 """add pjsip endpoints
 
 Revision ID: a28974a2dc19
-Revises: 5c9f286599b6
+Revises: 106159225d65
 
 """
 
@@ -12,7 +12,7 @@ from sqlalchemy.dialects.postgresql import UUID
 
 # revision identifiers, used by Alembic.
 revision = 'a28974a2dc19'
-down_revision = '5c9f286599b6'
+down_revision = '106159225d65'
 
 UUID_GEN = 'uuid_generate_v4()'
 

524ea2c8a031db7188eece2e219ff635497e0439
Pascal Cadotte Michaud <pcm@wazo.io>
2021-07-20 09:18:05 -0400

    call permission: make the name unique by tenant
    this is necessary to avoid name clashes that would break user imports

--- alembic/versions/907d4947d665_call_permission_non_unique_names.py
+++ alembic/versions/907d4947d665_call_permission_non_unique_names.py
@@ -13,12 +13,15 @@ revision = '907d4947d665'
 down_revision = 'cb5666745311'
 
 TBL_NAME = 'rightcall'
-CONSTRAINT_NAME = 'rightcall_name_key'
+OLD_CONSTRAINT_NAME = 'rightcall_name_key'
+NEW_CONSTRAINT_NAME = 'rightcall_name_tenant_uuid_key'
 
 
 def upgrade():
-    op.drop_constraint(CONSTRAINT_NAME, TBL_NAME)
+    op.create_unique_constraint(NEW_CONSTRAINT_NAME, TBL_NAME, ['name', 'tenant_uuid'])
+    op.drop_constraint(OLD_CONSTRAINT_NAME, TBL_NAME)
 
 
 def downgrade():
-    op.create_unique_constraint(CONSTRAINT_NAME, TBL_NAME, ['name'])
+    op.create_unique_constraint(OLD_CONSTRAINT_NAME, TBL_NAME, ['name'])
+    op.drop_constraint(NEW_CONSTRAINT_NAME, TBL_NAME)

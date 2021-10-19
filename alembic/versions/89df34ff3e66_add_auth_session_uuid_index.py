ebbe0f2cf12c9d45752402153fd89b860697efeb
Alexandre Fournier <afournier@wazo.io>
2021-10-19 15:01:23 -0400

    rename existing auth_token(session_uuid) index if already exist

--- alembic/versions/89df34ff3e66_add_auth_session_uuid_index.py
+++ alembic/versions/89df34ff3e66_add_auth_session_uuid_index.py
@@ -17,7 +17,20 @@ down_revision = '035908ce02df'
 def upgrade():
     # What we should be careful of: some production systems already have the index in place
     # since it was fixed by support
-    op.create_index('auth_token__idx__session_uuid', 'auth_token', ['session_uuid'])
+    idx_to_rename = {
+        'auth_token_session_uuid_idx': 'auth_token__idx__session_uuid',
+    }
+    conn = op.get_bind()
+    for idx, idx_renamed in idx_to_rename.items():
+        conn.execute(
+            sa.text(
+                f'ALTER INDEX IF EXISTS {idx} RENAME TO {idx_renamed};'
+            )
+        )
+
+    conn.execute(
+        sa.text('CREATE INDEX IF NOT EXISTS auth_token__idx__session_uuid ON auth_token (session_uuid);')
+    )
 
 
 def downgrade():

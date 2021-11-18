d9cba75cc2aaf4b24e7b11a6e2b17fa5d28fa3b0
Pascal Cadotte Michaud <pcm@wazo.io>
2021-11-18 12:01:51 -0500

    meeting: add the persistent column to meetings

---
+++ alembic/versions/51ebe7954da8_meeting_persistent_column.py
@@ -0,0 +1,26 @@
+"""meeting persistent column
+
+Revision ID: 51ebe7954da8
+Revises: ce76c220eaab
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51ebe7954da8'
+down_revision = 'ce76c220eaab'
+
+
+TABLE_NAME = 'meeting'
+COLUMN_NAME = 'persistent'
+
+
+def upgrade():
+    op.add_column(TABLE_NAME, sa.Column(COLUMN_NAME, sa.Boolean, nullable=False, default=False))
+
+
+def downgrade():
+    op.drop_column(TABLE_NAME, COLUMN_NAME)

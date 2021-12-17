2101dbbf931792153e079996ed346136543b4093
Pascal Cadotte Michaud <pcm@wazo.io>
2021-12-17 14:10:19 -0500

    meeting: fix the number migration script
    the old version failed if existing meeting existed

--- alembic/versions/b7d3c4701095_add_a_meeting_number.py
+++ alembic/versions/b7d3c4701095_add_a_meeting_number.py
@@ -18,12 +18,27 @@ down_revision = '2b51ff81d388'
 TABLE_NAME = 'meeting'
 COLUMN_NAME = 'number'
 
+
+meeting_tbl = sa.sql.table(
+    TABLE_NAME,
+    sa.column('uuid'),
+    sa.column('number'),
+)
+
 def random_number(length=6):
     return str(random.randint(0, int('9' * length))).rjust(length, '0')
 
 
 def upgrade():
-    op.add_column(TABLE_NAME, sa.Column(COLUMN_NAME, sa.Text, nullable=False, default=random_number))
+    op.add_column(TABLE_NAME, sa.Column(COLUMN_NAME, sa.Text))
+
+    query = sa.sql.select([meeting_tbl.c.uuid])
+    for row in op.get_bind().execute(query):
+        op.execute(
+            meeting_tbl.update().where(meeting_tbl.c.uuid==row.uuid).values(number=random_number())
+        )
+
+    op.alter_column(TABLE_NAME, COLUMN_NAME, nullable=False)
 
 
 def downgrade():

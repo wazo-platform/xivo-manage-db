f56bf502964620d080487f60a8e5ba6bf3ba2204
Jesse Sopel <jesse.sopel@wazo.io>
2023-04-03 10:35:00 -0400

    Add type checking and lint to xivo_db

--- xivo_db/check_db.py
+++ xivo_db/check_db.py
@@ -1,8 +1,8 @@
-# Copyright 2014-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2014-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_db import alembic
 
 
-def main():
+def main() -> None:
     alembic.check_db()

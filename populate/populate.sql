45b5d45aa0437e86821c184eac4e8fc06ced002d
Francois Blackburn <blackburnfrancois@gmail.com>
2023-05-03 14:38:09 -0400

    bump version (23.07)

--- populate/populate.sql
+++ populate/populate.sql
@@ -579,6 +579,6 @@ INSERT INTO "provisioning" VALUES(DEFAULT, '', 0, 8667);
 
 /* The UUID "populate-uuid" will be replaced by pg-populate-db */
 /* The version is bumped automatically during the release process */
-INSERT INTO "infos" (uuid, wazo_version, live_reload_enabled, timezone, configured) VALUES ('populate-uuid', '23.06', 'True', 'Europe/Paris', 'False');
+INSERT INTO "infos" (uuid, wazo_version, live_reload_enabled, timezone, configured) VALUES ('populate-uuid', '23.07', 'True', 'Europe/Paris', 'False');
 
 COMMIT;

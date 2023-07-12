9b4c54237c5f1a35727344b427241b71644a0362
billou <nabil.benyoussef@wazo.io>
2023-07-12 15:46:06 +0200

    Remove unuseful DELETE as fk+relationship already in place

--- alembic/versions/52d8c306e35f_add_fks_user_line.py
+++ alembic/versions/52d8c306e35f_add_fks_user_line.py
@@ -14,8 +14,6 @@ down_revision = '15389f63cfdd'
 
 
 def upgrade():
-    op.execute('DELETE FROM user_line WHERE user_id NOT IN (SELECT DISTINCT id FROM userfeatures)')
-    op.execute('DELETE FROM linefeatures WHERE id NOT IN (SELECT DISTINCT line_id FROM user_line)')
     op.drop_constraint("user_line_user_id_fkey", "user_line", type_="foreignkey")
     op.drop_constraint("user_line_line_id_fkey", "user_line", type_="foreignkey")
     op.create_foreign_key(

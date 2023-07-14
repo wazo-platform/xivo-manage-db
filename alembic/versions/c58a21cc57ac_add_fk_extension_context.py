9644f1e2f19da8ffee27d5688f8a8b36d3e7370b
billou <nabil.benyoussef@wazo.io>
2023-07-14 22:38:40 +0200

    Add fk between extension and context

---
+++ alembic/versions/c58a21cc57ac_add_fk_extension_context.py
@@ -0,0 +1,29 @@
+"""add_fk_extension_context
+
+Revision ID: c58a21cc57ac
+Revises: 15389f63cfdd
+
+"""
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'c58a21cc57ac'
+down_revision = '15389f63cfdd'
+
+
+def upgrade():
+    op.execute('DELETE FROM extensions WHERE context NOT IN (SELECT DISTINCT name FROM context)')
+    op.create_foreign_key(
+        None,
+        "extensions",
+        "context",
+        ["context"],
+        ["name"],
+        ondelete="CASCADE",
+    )
+
+
+def downgrade():
+    op.drop_constraint("extensions_context_fkey", "extensions", type_="foreignkey")

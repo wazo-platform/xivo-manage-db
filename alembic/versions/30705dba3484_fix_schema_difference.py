a1c978f831507d0d987832e48a5c917e943aa597
Pascal Cadotte Michaud <pcm@wazo.io>
2023-08-15 11:20:30 -0400

    extension features: fix missing index

--- alembic/versions/30705dba3484_fix_schema_difference.py
+++ alembic/versions/30705dba3484_fix_schema_difference.py
@@ -20,8 +20,54 @@ def upgrade():
     op.alter_column('func_key_dest_groupmember', 'feature_extension_uuid', nullable=False)
     op.alter_column('func_key_dest_service', 'feature_extension_uuid', nullable=False)
 
+    op.create_unique_constraint(
+        'feature_extension_exten_key',
+        'feature_extension',
+        ['exten'],
+    )
+
+    op.create_unique_constraint(
+        'func_key_dest_agent_agent_id_feature_extension_uuid_key',
+        'func_key_dest_agent',
+        ['agent_id', 'feature_extension_uuid'],
+    )
+    op.create_unique_constraint(
+        'func_key_dest_groupmember_group_id_feature_extension_uuid_key',
+        'func_key_dest_groupmember',
+        ['group_id', 'feature_extension_uuid'],
+    )
+    op.create_primary_key(
+        'func_key_dest_forward_pkey',
+        'func_key_dest_forward',
+        ['func_key_id', 'destination_type_id', 'feature_extension_uuid'],
+    )
+    op.create_primary_key(
+        'func_key_dest_service_pkey',
+        'func_key_dest_service',
+        ['func_key_id', 'destination_type_id', 'feature_extension_uuid'],
+    )
+
 
 def downgrade():
+    op.drop_constraint(
+        'func_key_dest_service_pkey',
+        'func_key_dest_service',
+    )
+    op.drop_constraint(
+        'func_key_dest_groupmember_group_id_feature_extension_uuid_key',
+        'func_key_dest_groupmember',
+    )
+    op.drop_constraint(
+        'func_key_dest_forward_pkey',
+        'func_key_dest_forward',
+    )
+    op.drop_constraint(
+        'func_key_dest_agent_agent_id_feature_extension_uuid_key',
+        'func_key_dest_agent',
+    )
+
+    op.drop_constraint('feature_extension_exten_key', 'feature_extension')
+
     op.alter_column('func_key_dest_agent', 'feature_extension_uuid', nullable=True)
     op.alter_column('func_key_dest_forward', 'feature_extension_uuid', nullable=True)
     op.alter_column('func_key_dest_groupmember', 'feature_extension_uuid', nullable=True)

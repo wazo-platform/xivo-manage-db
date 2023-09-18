055c235c663d5fddd0a5ed9c2b7f5b1d33787501
billou <nabil.benyoussef@wazo.io>
2023-09-18 20:35:47 +0200

    Add missing cascade deletion

---
+++ alembic/versions/ee2692520bd4_add_delete_cascade_schedule_schedulepath.py
@@ -0,0 +1,85 @@
+"""add_delete_cascade_schedule_schedulepath.py
+
+Revision ID: ee2692520bd4
+Revises: 71a9a51925d0
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'ee2692520bd4'
+down_revision = '71a9a51925d0'
+
+
+def upgrade():
+    op.drop_constraint("schedule_path_schedule_id_fkey", "schedule_path", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "schedule_path",
+        "schedule",
+        ["schedule_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+    op.drop_constraint("outcalltrunk_outcallid_fkey", "outcalltrunk", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "outcalltrunk",
+        "outcall",
+        ["outcallid"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+    op.drop_constraint("ivr_choice_ivr_id_fkey", "ivr_choice", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "ivr_choice",
+        "ivr",
+        ["ivr_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+    op.drop_constraint("rightcallexten_rightcallid_fkey", "rightcallexten", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "rightcallexten",
+        "rightcall",
+        ["rightcallid"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+def downgrade():
+    op.drop_constraint("schedule_path_schedule_id_fkey", "schedule_path", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "schedule_path",
+        "schedule",
+        ["schedule_id"],
+        ["id"],
+    )
+    op.drop_constraint("outcalltrunk_outcallid_fkey", "outcalltrunk", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "outcalltrunk",
+        "outcall",
+        ["outcallid"],
+        ["id"],
+    )
+    op.drop_constraint("ivr_choice_ivr_id_fkey", "ivr_choice", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "ivr_choice",
+        "ivr",
+        ["ivr_id"],
+        ["id"],
+    )
+    op.drop_constraint("rightcallexten_rightcallid_fkey", "rightcallexten", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "rightcallexten",
+        "rightcall",
+        ["rightcallid"],
+        ["id"],
+    )

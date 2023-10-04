07597aa121adb4c31d085ebc74d9f635b95b7a63
billou <nabil.benyoussef@wazo.io>
2023-10-04 22:03:26 +0200

    Fix query

--- alembic/versions/30fc2693c6fc_remove_default_moh.py
+++ alembic/versions/30fc2693c6fc_remove_default_moh.py
@@ -14,10 +14,12 @@ down_revision = '3a893dbde0e0'
 
 
 def upgrade():
-    op.execute('DELETE FROM moh WHERE name="default" and label="default"')
+    op.execute("""
+    DELETE FROM moh WHERE name='default' and label='default'
+    """)
 
 def downgrade():
     op.execute("""
     INSERT INTO moh (uuid, name, label, mode, tenant_uuid) 
-    VALUES(uuid_generate_v4(), 'default', 'default', 'files', (SELECT uuid from tenant WHERE slug = 'master'));
+    VALUES(uuid_generate_v4(), 'default', 'default', 'files', (SELECT uuid from tenant WHERE slug = 'master'))
     """)

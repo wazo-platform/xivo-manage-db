5d64413ff9ba883f404274fb0f3953b13a339281
billou <nabil.benyoussef@wazo.io>
2023-11-01 15:46:12 +0100

    Add ondelete cascade on func_key_dest_conference

---
+++ alembic/versions/d61fc6cc13c0_add_ondelete_cascade_funckey_dest_.py
@@ -0,0 +1,35 @@
+"""add_ondelete_cascade_funckey_dest_conference
+
+Revision ID: d61fc6cc13c0
+Revises: 11aa576dba87
+
+"""
+
+from alembic import op
+
+revision = 'd61fc6cc13c0'
+down_revision = '11aa576dba87'
+
+
+def upgrade():
+    op.execute("""
+        ALTER TABLE func_key_dest_conference 
+        DROP CONSTRAINT func_key_dest_conference_conference_id_fkey CASCADE""")
+
+    op.execute("""
+        ALTER TABLE func_key_dest_conference
+        ADD CONSTRAINT func_key_dest_conference_conference_id_fkey
+        FOREIGN KEY (conference_id)
+        REFERENCES conference(id)
+        ON DELETE CASCADE""")
+
+def downgrade():
+    op.execute("""
+        ALTER TABLE func_key_dest_conference 
+        DROP CONSTRAINT func_key_dest_conference_conference_id_fkey CASCADE""")
+
+    op.execute("""
+        ALTER TABLE func_key_dest_conference
+        ADD CONSTRAINT func_key_dest_conference_conference_id_fkey
+        FOREIGN KEY (conference_id)
+        REFERENCES conference(id)""")

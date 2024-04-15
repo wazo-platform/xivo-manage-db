b2b746d8340f103ca637d22306de93b91c474ee6
Charles Langlois <charles.langlois@wazo.io>
2024-04-15 14:43:47 -0400

    remove_incall_extensions_in_limbo: context.contexttype to filter incall extensions, directly filtered and joined DELETE operation

--- alembic/versions/b38ce53a1d32_remove_incall_extensions_in_limbo.py
+++ alembic/versions/b38ce53a1d32_remove_incall_extensions_in_limbo.py
@@ -1,6 +1,6 @@
 """remove-extensions-in-limbo
 
-remove extensions from table extensions in *from-extern contexts associated with user 0
+remove extensions from table extensions from incall contexts associated with user 0,
 which are incall extensions disassociated from a real destination but not available for new assignments
 Revision ID: b38ce53a1d32
 Revises: 7a7f7c44f943
@@ -10,10 +10,10 @@ Revises: 7a7f7c44f943
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = 'b38ce53a1d32'
 down_revision = '7a7f7c44f943'
+
 
 extensions_table = sa.table(
     'extensions',
@@ -25,16 +25,22 @@ extensions_table = sa.table(
 )
 
 
+context_table = sa.table(
+    'context',
+    sa.column('id'),
+    sa.column('name'),
+    sa.column('contexttype'),
+)
+
+
 def upgrade() -> None:
-    query = sa.sql.select([extensions_table.c.id]).where(
+    delete_op = extensions_table.delete().where(
         sa.and_(
+            extensions_table.c.context == context_table.c.name,
             extensions_table.c.type == 'user',
             extensions_table.c.typeval == '0',
-            extensions_table.c.context.ilike('%from-extern')
+            context_table.c.contexttype == 'incall'
         )
-    )
-    delete_op = extensions_table.delete().where(
-        extensions_table.c.id.in_(query.alias())
     )
     op.get_bind().execute(delete_op)
 

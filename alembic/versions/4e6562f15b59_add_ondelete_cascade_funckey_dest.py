c4c7eae94177d3106284e14a8b26b4a5edadcaa3
Alexandre Fournier <afournier@wazo.io>
2025-04-17 11:37:54 -0400

    func_key_dest_park_position: add cascade on parking_lot_id foreign key

--- alembic/versions/4e6562f15b59_add_ondelete_cascade_funckey_dest.py
+++ alembic/versions/4e6562f15b59_add_ondelete_cascade_funckey_dest.py
@@ -97,6 +97,16 @@ def upgrade():
         ondelete="CASCADE",
     )
 
+    op.drop_constraint("func_key_dest_park_position_parking_lot_id_fkey", "func_key_dest_park_position", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "func_key_dest_park_position",
+        "parking_lot",
+        ["parking_lot_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
 
 def downgrade():
     op.drop_constraint("func_key_dest_user_user_id_fkey", "func_key_dest_user", type_="foreignkey")
@@ -168,5 +178,14 @@ def downgrade():
         "func_key_dest_paging",
         "paging",
         ["paging_id"],
+        ["id"],
+    )
+
+    op.drop_constraint("func_key_dest_park_position_parking_lot_id_fkey", "func_key_dest_park_position", type_="foreignkey")
+    op.create_foreign_key(
+        None,
+        "func_key_dest_park_position",
+        "parking_lot",
+        ["parking_lot_id"],
         ["id"],
     )

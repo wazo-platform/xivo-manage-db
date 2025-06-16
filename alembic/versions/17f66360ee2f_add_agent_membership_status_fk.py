dcbe0ba363f1783b325fb59e8df0d0e7a506be9d
Pascal Cadotte Michaud <pcm@wazo.io>
2025-06-16 09:56:44 -0400

    agent_membership_status: make agent_id a FK

---
+++ alembic/versions/17f66360ee2f_add_agent_membership_status_fk.py
@@ -0,0 +1,34 @@
+"""add agent-membership-status fk
+
+Revision ID: 17f66360ee2f
+Revises: 3131b2ccb06f
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '17f66360ee2f'
+down_revision = '3131b2ccb06f'
+
+
+def upgrade():
+    op.create_foreign_key(
+        None,
+        "agent_membership_status",
+        "agentfeatures",
+        ["agent_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+
+def downgrade():
+    op.drop_constraint(
+        "agent_membership_status_agent_id_fkey",
+        "agent_membership_status",
+        type_="foreignkey",
+    )

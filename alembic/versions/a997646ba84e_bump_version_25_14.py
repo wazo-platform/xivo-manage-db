7f53d5894d242755af70d1cbe8fb814c2b849d53
Remy Bienvenu <remy.bienvenu@wazo.io>
2025-09-22 14:23:00 -0700

    bump version (25.14)

---
+++ alembic/versions/a997646ba84e_bump_version_25_14.py
@@ -0,0 +1,24 @@
+"""bump_version_25_14
+
+Revision ID: a997646ba84e
+Revises: c17681419c6e
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'a997646ba84e'
+down_revision = 'c17681419c6e'
+
+
+def upgrade():
+    infos = sa.sql.table('infos', sa.sql.column('wazo_version'))
+    op.execute(infos.update().values(wazo_version='25.14'))
+
+
+def downgrade():
+    pass

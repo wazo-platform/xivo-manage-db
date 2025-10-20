7d114376c7a62b560a3deb0c1897756bb821ded4
Remy Bienvenu <remy.bienvenu@wazo.io>
2025-10-20 14:16:02 -0700

    voicemail: Add shared field
    WP-2044

---
+++ alembic/versions/f16fdc26c6c6_add_shared_field_to_voicemails.py
@@ -0,0 +1,25 @@
+"""Add shared field to voicemails
+
+Revision ID: f16fdc26c6c6
+Revises: 22b03291f3ac
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'f16fdc26c6c6'
+down_revision = '22b03291f3ac'
+
+
+def upgrade():
+    op.add_column('voicemail', sa.Column('shared', sa.Boolean, nullable=False, server_default=sa.text('false')))
+    op.create_index('voicemail_shared_tenant_unique_key', 'voicemail', ['shared','context'], unique=True, postgresql_where=('shared'))
+
+
+def downgrade():
+    op.drop_index('voicemail_shared_tenant_unique_key', 'voicemail')
+    op.drop_column('voicemail', 'shared')

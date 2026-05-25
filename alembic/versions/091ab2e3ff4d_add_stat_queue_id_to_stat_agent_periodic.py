ab531bb66de724f3d6ab4f988993683468fd8da1
Remy Bienvenu <remy.bienvenu@wazo.io>
2026-05-25 12:58:02 -0700

    migration: rebase migration on latest change
    ITEM-295

--- alembic/versions/091ab2e3ff4d_add_stat_queue_id_to_stat_agent_periodic.py
+++ alembic/versions/091ab2e3ff4d_add_stat_queue_id_to_stat_agent_periodic.py
@@ -1,7 +1,7 @@
 """add stat_queue_id to stat_agent_periodic
 
 Revision ID: 091ab2e3ff4d
-Revises: 0dd9e46ce2bc
+Revises: cb49d48129fd
 
 """
 
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '091ab2e3ff4d'
-down_revision = '0dd9e46ce2bc'
+down_revision = 'cb49d48129fd'
 
 
 def upgrade():
